83a241640e4c6cd69027a4a89f9eb7828e1c6b22
CapyDev123 <joao.gabriel.siqueira@escola.pr.gov.br>
2025-11-03 09:28:27 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.py
@@ -0,0 +1,9 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+def on_button_pressed_a():
+    pass
+sprite1 = game.create_sprite(0, 2)
+if sprite1 == sprite1:
+ input.on_button_pressed(Button.A, on_button_pressed_a)
+ sprite1.delete()
---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function on_forever() {
+    
+})
+let sprite1 = game.createSprite(0, 2)
+if (sprite1 == sprite1) {
+    input.onButtonPressed(Button.A, function on_button_pressed_a() {
+        
+    })
+    sprite1.delete()
+}
+
